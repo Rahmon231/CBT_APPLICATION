a8c1c2cc6904d101effe76e5398b301d652183bc
Akanbi Rahmon <Rahmon.Akanbi@outlook.com>
2022-05-02 22:57:45 +0100

    adding sound to the splash activity

--- app/src/main/java/com/example/cbtapplication/SplashActivity.java
+++ app/src/main/java/com/example/cbtapplication/SplashActivity.java
@@ -7,6 +7,8 @@ import androidx.core.content.res.ResourcesCompat;
 
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.media.AudioAttributes;
+import android.media.SoundPool;
 import android.os.Bundle;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -14,26 +16,40 @@ import android.widget.TextView;
 
 public class SplashActivity extends AppCompatActivity {
     private TextView appName;
+    private SoundPool soundPool;
+    private int sound;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         appName = findViewById(R.id.appName);
+        AudioAttributes audioAttributes = new AudioAttributes.Builder()
+                .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                .setUsage(AudioAttributes.USAGE_ASSISTANCE_SONIFICATION)
+                .build();
+        soundPool = new SoundPool.Builder()
+                .setMaxStreams(1)
+                .setAudioAttributes(audioAttributes)
+                .build();
+        sound = soundPool.load(this,R.raw.correct,1);
         Typeface typeface = ResourcesCompat.getFont(this,R.font.blacklist);
         appName.setTypeface(typeface);
         Animation anim = AnimationUtils.loadAnimation(this,R.anim.myanim);
         appName.setAnimation(anim);
 
+
         new Thread(new Runnable() {
             @Override
             public void run() {
                  try {
-                 sleep(3000);
+                     sleep(3000);
+                     soundPool.play(sound,1,1,0,0,1);
 
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
+                //soundPool.play(sound,1,1,0,0,1);
                 Intent intent = new Intent(SplashActivity.this,MainActivity.class);
                 startActivity(intent);
             }
